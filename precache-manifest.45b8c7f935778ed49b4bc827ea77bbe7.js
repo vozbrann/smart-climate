0642e731b3a6bcb16cae459c0b57bfa6f3e12368
Roman Vozbrannyi <39827437+vozbrann@users.noreply.github.com>
2020-04-13 04:10:01 +0300

    Add files via upload

---
+++ precache-manifest.45b8c7f935778ed49b4bc827ea77bbe7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60aaaf85b76e57ba7bd4c399902f4aa3",
+    "url": "/smart-climate/index.html"
+  },
+  {
+    "revision": "ca29f89f0fe98502afed",
+    "url": "/smart-climate/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "ca29f89f0fe98502afed",
+    "url": "/smart-climate/static/js/2.d44f771f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/smart-climate/static/js/2.d44f771f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50e1a57b8177b41b892b",
+    "url": "/smart-climate/static/js/main.a2c12615.chunk.js"
+  },
+  {
+    "revision": "93bce8c81dd427efdc78",
+    "url": "/smart-climate/static/js/runtime-main.15338196.js"
+  },
+  {
+    "revision": "fae88e181b8059a404e779717429ce84",
+    "url": "/smart-climate/static/media/air-conditioner.fae88e18.svg"
+  },
+  {
+    "revision": "76d344f08412543c74ccd1fb2493d607",
+    "url": "/smart-climate/static/media/github-logo.76d344f0.svg"
+  },
+  {
+    "revision": "62a03e1390196674be3de73a5c0e052c",
+    "url": "/smart-climate/static/media/heater.62a03e13.svg"
+  },
+  {
+    "revision": "d900f7b83e56d75668077c7423662776",
+    "url": "/smart-climate/static/media/humidity.d900f7b8.svg"
+  },
+  {
+    "revision": "10a1d9caea507d6e9c621e5d40db87a7",
+    "url": "/smart-climate/static/media/weather.10a1d9ca.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.c44b43d330aa75d41c4d8797c16583d8.js"
+  "/smart-climate/precache-manifest.45b8c7f935778ed49b4bc827ea77bbe7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/smart-climate/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
