9b1b26bf479ebf4004139012124b940a6ce65b6c
Roman Vozbrannyi <39827437+vozbrann@users.noreply.github.com>
2020-04-13 04:05:33 +0300

    Add files via upload

---
+++ precache-manifest.c44b43d330aa75d41c4d8797c16583d8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9c49ddcdc33003ec7dab59e8748c62c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ca29f89f0fe98502afed",
+    "url": "./static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "ca29f89f0fe98502afed",
+    "url": "./static/js/2.d44f771f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.d44f771f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50e1a57b8177b41b892b",
+    "url": "./static/js/main.a2c12615.chunk.js"
+  },
+  {
+    "revision": "ed2b45ae3fe7c48d1c1f",
+    "url": "./static/js/runtime-main.ef43735b.js"
+  },
+  {
+    "revision": "fae88e181b8059a404e779717429ce84",
+    "url": "./static/media/air-conditioner.fae88e18.svg"
+  },
+  {
+    "revision": "76d344f08412543c74ccd1fb2493d607",
+    "url": "./static/media/github-logo.76d344f0.svg"
+  },
+  {
+    "revision": "62a03e1390196674be3de73a5c0e052c",
+    "url": "./static/media/heater.62a03e13.svg"
+  },
+  {
+    "revision": "d900f7b83e56d75668077c7423662776",
+    "url": "./static/media/humidity.d900f7b8.svg"
+  },
+  {
+    "revision": "10a1d9caea507d6e9c621e5d40db87a7",
+    "url": "./static/media/weather.10a1d9ca.svg"
+  }
+]);
